567406b8b4a1155cba99c0886a74d262335a4b65
Emma Fcx <etud@port-dell-latitude-3500-23>
2022-04-07 17:32:09 +0200

    Modification de la page accueil, essayer de tout mettre au millieu et ajout danimation en cours de developpement

---
+++ Accueil/app.js
@@ -0,0 +1,24 @@
+const titreSpans = document.querySelectorAll('h1 span');
+const btns = document.querySelectorAll('.btn-first');
+const btns1 = document.querySelectorAll('.btn-first:nth-child(1)');
+const btns2 = document.querySelectorAll('.btn-first:nth-child(2)');
+
+
+window.addEventListener('load', () => {
+
+    const TL = gsap.timeline({paused: true});
+    //(objet, duree de lobject {}, temps entre chaque objet)
+    TL
+    .staggerFrom(titreSpans, 1, {top: -100, opacity: 2}, 0.2)
+    //.staggerFrom(btns1, 1, {opacity: 2, ease: "power2.out"}, 0.2, '-=1')
+    .from(btns1, 1, {width: -50}, '-=2')   
+    //.from(btns2, 1, {right: -50, width: 10}, '-=2')
+    ;
+
+     //.staggerFrom(btns, 1, {opacity: 0}, 0.3, '-=1')
+    //.staggerFrom(btns2, 1, {top: -100, opacity: 0});
+
+    
+
+    TL.play();
+})
