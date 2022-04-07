215bf8c9bccb8def1056e97dab0ba14f00c59d52
Emma Fcx <etud@port-dell-latitude-3500-23>
2022-04-07 15:01:56 +0200

    Fichier html mis dans le php pour la page de connexion

--- Connexion/connexion.php
+++ Connexion/connexion.php
@@ -66,3 +66,48 @@
 </form>
 </body>
 </html>
+
+<!--
+<!DOCTYPE html>
+<html dir="ltr" lang="fr-FR">
+ entete 
+<head>
+    <meta charset="utf-8" />
+    <title>Page de connexionl</title>
+    <link rel="stylesheet" href="connexion.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+contenu de la page
+<body>
+
+    <button onclick="window.location.href = 'connexion.php'>Cliquez Ici </button> 
+
+<div class="page">
+    <div class="container">
+      <div class="left">
+        <div class="login">connexion</div>
+        <div class="eula">By logging in you agree to the ridiculously long terms that you didn't bother to read</div>
+      </div>
+      <div class="right">
+       
+
+        <div class="form">
+
+          <label for="tel">Téléphone : </label>
+          <input input type="tel" pattern="[0-9]{10}" maxlength="10"  id="tel" /> 
+
+          <label for="password">Mot de passe : </label>
+          <input type="password" id="password">
+
+          <input type="submit" id="submit" value="connexion">
+        </div>
+      </div>
+    </div>
+  </div>
+  
+   -Ajouter un lien de contact en bas de la page ?
+
+   <script src="app.js"></script> notre script.js
+</body>
+</html>-->
