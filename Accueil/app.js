e940235552caaab71e1e1e4c86336a5be2d617f7
Emma Fcx <etud@port-dell-latitude-3500-23>
2022-04-26 11:22:59 +0200

    Objectif merge emma2

--- Accueil/app.js
+++ Accueil/app.js
@@ -9,13 +9,16 @@ window.addEventListener('load', () => {
     const TL = gsap.timeline({paused: true});
     //(objet, duree de lobject {}, temps entre chaque objet)
     TL
-    .staggerFrom(titreSpans, 1, {top: -100, opacity: 2}, 0.2)
-    //.staggerFrom(btns1, 1, {opacity: 2, ease: "power2.out"}, 0.2, '-=1')
-    .from(btns1, 1, {width: -50}, '-=2')   
-    //.from(btns2, 1, {right: -50, width: 10}, '-=2')
-    ;
 
-     //.staggerFrom(btns, 1, {opacity: 0}, 0.3, '-=1')
+    //.staggerFrom(titreSpans, 1, {top: -100, opacity: 2}, 0.2)
+    //.staggerFrom(btns1, 1, {opacity: 2, ease: "power2.out"}, 0.2, '-=1')
+   $("titreSpans").fadeIn();
+   
+    //.from(btns1, 1, {width: -50}, '-=2')   ;
+    //.from(btns2, 1, {right: -50, width: 10}, '-=2')
+    
+
+    //.staggerFrom(btns, 1, {opacity: 0}, 0.3, '-=1')
     //.staggerFrom(btns2, 1, {top: -100, opacity: 0});
 
     
