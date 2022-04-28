87e3fcec4aaa0c56f4c04fe589a9ae8654fa8543
Emma Fcx <etud@port-dell-latitude-3500-23>
2022-04-28 10:07:14 +0200

    ajout des photos dans ressources test

--- Site/site.php
+++ Site/site.php
@@ -8,6 +8,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="site.css">
+    <link rel="stylesheet" href="../header.css">
+    <link rel="stylesheet" href="../footer.css">
+
+
     <title>Site principal</title>
 </head>
 
@@ -56,6 +60,9 @@
                     // echo "<pre>";
                     // var_dump($item);
                     // echo "</pre>";
+
+
+
 
                     echo "<div class='elementannonce'>
                     <div id='gauche'>
