b44b137ff02e68b95aba7b0827a55165cb6a27d7
Bertille Trotin <bertille.trotin@gmail.com>
2022-05-02 15:07:34 +0200

    formulaire annonce

--- Site/formulaireannonce.php
+++ Site/formulaireannonce.php
@@ -9,6 +9,11 @@
         <div id="contenu">
         <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post" enctype="application/x-www-form-urlencoded">    <!--Voir a quoi ca sert plus tars-->
                 <h1>Poster une annonce : </h1>
+
+                <p>
+                <label><b>Le titre : </b></label>
+                <input type="text" placeholder="Entrer un titre" maxlength="100"  name="titre" size="20" /> 
+                </p>
                 
                 <p>
                 <label><b>La ville : </b></label>
@@ -34,7 +39,6 @@
                 </br>
                 <label>Heure fin</label>
                 <input type="time" id="hfin" name="hfin" value="20:00" >
-                <!-- <input type="text" placeholder="Entrer une plage horaire" name="heure" size="20" required> -->
                 </p>
 
             
@@ -50,6 +54,8 @@
                     <option value="culture">Culture</option>
                     <option value="bar">Bar</option>
                     <option value="festival">Festival</option>
+                    <option value="loisir">Loisir</option>
+
                 </select>
                 
                 <select name="theme2">
@@ -63,6 +69,8 @@
                     <option value="culture">Culture</option>
                     <option value="bar">Bar</option>
                     <option value="festival">Festival</option>
+                    <option value="loisir">Loisir</option>
+
                 </select>
 
                 <select name="theme3">
@@ -76,6 +84,7 @@
                     <option value="culture">Culture</option>
                     <option value="bar">Bar</option>
                     <option value="festival">Festival</option>
+                    <option value="loisir">Loisir</option>
                     
                 </select>
                 </p>
@@ -98,12 +107,13 @@
 <?php
 
 
-include ("../BDD/connexionpdo.php");
-// include ("connexionpdo.php");
+// include ("../BDD/connexionpdo.php");
+include ("../Connexion/connexionpdo.php");
 
 session_start();
 
 $idcom = connexpdo("Projet");
+
 
 
 if($_SESSION['Num_Tel'] !== ""){
@@ -114,13 +124,12 @@ echo "Bonjour $propri, vous êtes connecté";
 // exit();
 }
 
-$idcom = connexpdo("Projet");
+// $idcom = connexpdo("Projet");
 
-
-
- if (! empty($_POST['ville']) && ! empty($_POST['lieu']) && ! empty($_POST['date']) && ! empty($_POST['hdebut']) && ! empty($_POST['hfin']) && ! empty($_POST['theme1']) ) {
-
+ if (! empty($_POST['titre']) && ! empty($_POST['ville']) && ! empty($_POST['lieu']) && ! empty($_POST['date']) && ! empty($_POST['hdebut']) && ! empty($_POST['hfin']) && ! empty($_POST['theme1']) ) {
     try {
+
+        $titre = $idcom->quote($_POST['titre']);
         $ville = $idcom->quote($_POST['ville']);
         $lieu = $idcom->quote($_POST['lieu']);
         $date = $idcom->quote($_POST['date']);
@@ -131,12 +140,11 @@ $idcom = connexpdo("Projet");
         $theme3 = $idcom->quote($_POST['theme3']);
         $infosup = $idcom->quote($_POST['infosup']);
 
-        $not = "not";
 
-            if (!($not == $_POST['theme2']) && !($not == $_POST['theme3']) && ! empty($_POST['infosup']) ) {
+            if (("not" != $_POST['theme2']) && ("not" != $_POST['theme3']) && ! empty($_POST['infosup']) ) {
 
 
-                $query = "INSERT INTO Annonces VALUES($ville,$lieu,$date,$hdebut,$hfin,$theme1,$theme2,$theme3,$infosup,$propri)";
+                $query = "INSERT INTO Annonces VALUES($ville,$lieu,$date,$hdebut,$hfin,$theme1,$theme2,$theme3,$infosup,$propri,$titre)";
                 $nb = $idcom->exec($query);
                 if ($nb != 1) {
                     alert("Erreur : \"$idcom->errorCode()\"");
@@ -145,9 +153,27 @@ $idcom = connexpdo("Projet");
                 }
             }
 
-            if (($not == $_POST['theme2']) && ($not == $_POST['theme3']) && empty($_POST['infosup']) ) {
+            else{
+                echo "<h3>PROBLEME 1!! </h3>";
+            }
 
-                    $query = "INSERT INTO Annonces VALUES($ville,$lieu,$date,$hdebut,$hfin,$theme1,NULL,NULL,NULL,$propri)";
+            if (("not" == $_POST['theme2']) && ("not" == $_POST['theme3']) && empty($_POST['infosup']) ) {
+
+                    $query = "INSERT INTO Annonces VALUES($ville,$lieu,$date,$hdebut,$hfin,$theme1,NULL,NULL,NULL,$propri,$titre)";
+                    $nb = $idcom->exec($query);
+                    if ($nb != 1) {
+                        alert("Erreur : \"$idcom->errorCode()\"");
+                    }else {
+                        alert("Modèle bien enregistré !");
+                    }
+                }
+            else{
+                    echo "<h3>PROBLEME 2!! </h3>";
+                }
+
+            if (("not" == $_POST['theme2']) && ("not" != $_POST['theme3']) && ! empty($_POST['infosup']) ){
+                    
+                    $query = "INSERT INTO Annonces VALUES($ville,$lieu,$date,$hdebut,$hfin,$theme1,NULL,$theme3,$infosup,$propri,$titre)";
                     $nb = $idcom->exec($query);
                     if ($nb != 1) {
                         alert("Erreur : \"$idcom->errorCode()\"");
@@ -156,9 +182,13 @@ $idcom = connexpdo("Projet");
                     }
                 }
 
-            if (($not == $_POST['theme2']) && !($not == $_POST['theme3']) && ! empty($_POST['infosup']) ){
+                else{
+                    echo "<h3>PROBLEME 3!! </h3>";
+                }
+
+            if (("not" != $_POST['theme2']) && ("not" == $_POST['theme3']) && ! empty($_POST['infosup']) ){
                     
-                    $query = "INSERT INTO Annonces VALUES($ville,$lieu,$date,$hdebut,$hfin,$theme1,NULL,$theme3,$infosup,$propri)";
+                    $query = "INSERT INTO Annonces VALUES($ville,$lieu,$date,$hdebut,$hfin,$theme1,$theme2,NULL,$infosup,$propri,$titre)";
                     $nb = $idcom->exec($query);
                     if ($nb != 1) {
                         alert("Erreur : \"$idcom->errorCode()\"");
@@ -167,9 +197,13 @@ $idcom = connexpdo("Projet");
                     }
                 }
 
-            if (!($not == $_POST['theme2']) && ($not == $_POST['theme3']) && ! empty($_POST['infosup']) ){
+                else{
+                    echo "<h3>PROBLEME 4!! </h3>";
+                }
+
+            if (("not" != $_POST['theme2']) && ("not" != $_POST['theme3']) && empty($_POST['infosup']) ){
                     
-                    $query = "INSERT INTO Annonces VALUES($ville,$lieu,$date,$hdebut,$hfin,$theme1,$theme2,NULL,$infosup,$propri)";
+                    $query = "INSERT INTO Annonces VALUES($ville,$lieu,$date,$hdebut,$hfin,$theme1,$theme2,$theme3,NULL,$propri,$titre)";
                     $nb = $idcom->exec($query);
                     if ($nb != 1) {
                         alert("Erreur : \"$idcom->errorCode()\"");
@@ -178,10 +212,50 @@ $idcom = connexpdo("Projet");
                     }
                 }
 
-            if (!($not == $_POST['theme2']) && !($not == $_POST['theme3']) && empty($_POST['infosup']) ){
+                else{
+                    echo "<h3>PROBLEME 5 !! </h3>";
+                }
+                
+            if (("not" == $_POST['theme2']) && ("not" == $_POST['theme3']) && ! empty($_POST['infosup']) ){
                     
-                    $query = "INSERT INTO Annonces VALUES($ville,$lieu,$date,$hdebut,$hfin,$theme1,$theme2,$theme3,NULL,$propri)";
+                    $query = "INSERT INTO Annonces VALUES($ville,$lieu,$date,$hdebut,$hfin,$theme1,NULL,NULL,$infosup,$propri,$titre)";
                     $nb = $idcom->exec($query);
+                    if ($nb != 1) {
+                        alert("Erreur : \"$idcom->errorCode()\"");
+                    }else {
+                        alert("Modèle bien enregistré !");
+                    }
+                }
+
+                else{
+                    echo "<h3>PROBLEME 6!! </h3>";
+                }
+
+            if (("not"== $_POST['theme2']) && ("not" != $_POST['theme3']) && empty($_POST['infosup']) ){
+                    
+                    $query = "INSERT INTO Annonces VALUES($ville,$lieu,$date,$hdebut,$hfin,$theme1,NULL,$theme3,NULL,$propri,$titre)";
+                    $nb = $idcom->exec($query);
+                    if ($nb != 1) {
+                        alert("Erreur : \"$idcom->errorCode()\"");
+                    }else {
+                        alert("Modèle bien enregistré !");
+                    }
+                }
+
+
+                else{
+                    echo "<h3>PROBLEME 7!! </h3>";
+                }
+
+
+            if (("not" != $_POST['theme2']) && ("not" == $_POST['theme3']) && empty($_POST['infosup']) ){
+                    
+                echo "<h3>test 8!! </h3>";
+
+                    $query = "INSERT INTO Annonces VALUES($ville,$lieu,$date,$hdebut,$hfin,$theme1,$theme2,NULL,NULL,$propri,$titre)";
+                    $nb = $idcom->exec($query);
+
+                    echo "<h3>test 8 bis !! </h3>";
                     if ($nb != 1) {
                         alert("Erreur : \"$idcom->errorCode()\"");
                     }else {
@@ -189,38 +263,9 @@ $idcom = connexpdo("Projet");
                     }
                 }
                 
-            if (($not == $_POST['theme2']) && ($not == $_POST['theme3']) && ! empty($_POST['infosup']) ){
-                    
-                    $query = "INSERT INTO Annonces VALUES($ville,$lieu,$date,$hdebut,$hfin,$theme1,NULL,NULL,$infosup,$propri)";
-                    $nb = $idcom->exec($query);
-                    if ($nb != 1) {
-                        alert("Erreur : \"$idcom->errorCode()\"");
-                    }else {
-                        alert("Modèle bien enregistré !");
-                    }
-                }
-
-            if (($not == $_POST['theme2']) && !($not == $_POST['theme3']) && empty($_POST['infosup']) ){
-                    
-                    $query = "INSERT INTO Annonces VALUES($ville,$lieu,$date,$hdebut,$hfin,$theme1,NULL,$theme3,NULL,$propri)";
-                    $nb = $idcom->exec($query);
-                    if ($nb != 1) {
-                        alert("Erreur : \"$idcom->errorCode()\"");
-                    }else {
-                        alert("Modèle bien enregistré !");
-                    }
-                }
-
-            if (!($not == $_POST['theme2']) && ($not == $_POST['theme3']) && empty($_POST['infosup']) ){
-                    
-                    $query = "INSERT INTO Annonces VALUES($ville,$lieu,$date,$hdebut,$hfin,$theme1,$theme2,NULL,NULL,$propri)";
-                    $nb = $idcom->exec($query);
-                    if ($nb != 1) {
-                        alert("Erreur : \"$idcom->errorCode()\"");
-                    }else {
-                        alert("Modèle bien enregistré !");
-                    }
-                }     
+            else{
+                echo "<h3>PROBLEME 8!! </h3>";
+            }
             
 
     } catch (PDOException $e) {
