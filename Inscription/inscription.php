a9ac7aa9fe7f075079ce7517272e9d665397d735
Emma Fcx <etud@port-dell-latitude-3500-23>
2022-05-03 16:56:50 +0200

    Suppression de inscription.html et remplacer par .php, ainsi que ajout de traitement inscription pour plus de visibilité

--- Inscription/inscription.php
+++ Inscription/inscription.php
@@ -9,9 +9,10 @@
 </head>
 <body>
 
-<?php
-include("../header.php");
-?>
+    <?php
+    include("../header.php");
+    include("trtInscription.php");
+    ?>
 
 <div id=contenu>
     <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="application/x-www-form-urlencoded">
@@ -60,42 +61,5 @@ include("../header.php");
         </fieldset>
     </form>
 </div>
-<?php
-include("../BDD/connexionpdo.php");
-$idcom = connexpdo("Projet");
-if(!empty($_POST['nom']) && !empty($_POST['prenom']) && !empty($_POST['tel']) && !empty($_POST['age']) && !empty($_POST['mdp'])) {
-    try {
-        $nom = $idcom -> quote($_POST['nom']);
-        $prenom = $idcom -> quote($_POST['prenom']);
-        $tel = $idcom -> quote($_POST['tel']);
-        $age = $idcom -> quote($_POST['age']);
-        $mdp = $idcom -> quote($_POST['mdp']);
-        $mdpconfirm = $idcom -> quote($_POST['mdpconfirm']);
-        $pseudo = $idcom -> quote($_POST['pseudo']);
-        $ville = $idcom -> quote($_POST['ville']);
-
-        if($mdp != $mdpconfirm){
-            alert("Erreur : Le mot de passe ne correspond pas ! ");
-            return;
-        }
-
-        $query = "INSERT INTO utilisateurs VALUES($nom,$prenom,$pseudo,$age,$tel,$ville,$mdp)";
-        $nb = $idcom -> exec($query);
-        if($nb != 1) {
-            alert("Erreur : \"$idcom->errorCode()\"");
-        } else {
-            // alert("Modèle bien enregistré !");
-            header('Location: ../Connexion/connexion.php');
-            exit();
-
-        }
-    } catch(PDOException $e) {
-        displayException($e);
-        exit();
-    }
-} else {
-    echo "<h3>Formulaire à compléter !</h3>";
-}
-?>
 </body>
 </html>
